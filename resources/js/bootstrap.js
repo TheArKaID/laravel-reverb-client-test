e918e420b91562fca12b0187490f9d8ee557471f
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-25 11:31:35 +0700

    Initial App

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -27,7 +27,8 @@ import "jquery.nicescroll";
 // import "bootstrap-tagsinput";
 // import "select2";
 // import "selectric";
-// import "codemirror";
+import CodeMirror from "codemirror";
+window.CodeMirror = CodeMirror
 // import "fullcalendar";
 // import "datatables";
 // import "sweetalert";
@@ -59,17 +60,34 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Pusher from 'pusher-js';
+window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+    broadcaster: `${window.echoConfig.broadcaster}`,
+    key: `${window.echoConfig.key}`,
+    wsHost: `${window.echoConfig.host}`,
+    wsPort: `${window.echoConfig.port}`,
+    wssPort: `${window.echoConfig.port}`,
+    forceTLS: window.echoConfig.forceTLS,
+    enabledTransports: ['ws', 'wss'],
+});
+// Access the Pusher instance
+const pusher = window.Echo.connector.pusher;
+
+// Listen for the 'connected' event
+pusher.connection.bind('connected', () => {
+    console.log('WebSocket connected');
+});
+
+// Listen for the 'disconnected' event
+pusher.connection.bind('disconnected', () => {
+    console.log('WebSocket disconnected');
+});
+
+// Listen for the 'error' event
+pusher.connection.bind('error', (err) => {
+    console.error('WebSocket error:', err);
+});
--- resources/views/pages/blank-page.blade.php
+++ resources/views/pages/blank-page.blade.php
@@ -9,16 +9,95 @@
 @section('main')
     <div class="main-content">
         <section class="section">
-            <div class="section-header">
-                <h1>Blank Page</h1>
+            <div class="section-header" style="display: inherit;">
+                <h1>Pusher Compatible Tester</h1>
+                <br>
+                <small>Designed for Laravel Websocket Testing</small>
             </div>
 
             <div class="section-body">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="channel">Channel Name</label>
+                                        <input type="text" id="channel" class="form-control" placeholder="Channel Name">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="event">Event Name</label>
+                                        <input type="text" id="event" class="form-control" placeholder="Event Name">
+                                    </div>
+                                    <button class="btn btn-primary" onclick="listen()">Listen</button>
+                                </div>
+                            {{-- <div class="card-footer">
+                                <textarea class="form-control" id="data" rows="6" style="height: unset" hidden></textarea>
+                            </div> --}}
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-row">
+                                    <textarea class="form-control" id="data" rows="6" style="height: unset" hidden></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </div>
 @endsection
 
 @push('scripts')
-    <!-- JS -->
+    <script>
+        var channelName = ''
+        var eventName = ''
+        var data = ''
+
+        window.echoConfig = {
+            broadcaster: 'reverb',
+            key: 'grosphere-dev',
+            host: 'dev.grosphere.sg',
+            port: 443,
+            forceTLS: false
+        };
+
+        textarea = document.getElementById('data');
+        var editor
+        function listen() {
+            channelName = document.getElementById('channel');
+            eventName = document.getElementById('event');
+            textarea.hidden = false;
+            editor = window.CodeMirror.fromTextArea(textarea, {
+                lineNumbers: true,
+                mode: 'javascript',
+                json: true,
+                theme: 'duotone-dark',
+                readOnly: true,
+                scrollbarStyle: 'null',
+            });
+
+            var channel = Echo.channel(channelName.value);
+            console.log(channelName.value, eventName.value)
+            channel.listen(eventName.value, (data) => {
+                // append and new line
+                data = editor.getValue() + '\n' + JSON.stringify(data, null, 2);
+                editor.setValue(data);
+            });
+            channel.listenToAll((event, data) => {
+                // do what you need to do based on the event name and data
+                console.log(event, data)
+            });
+        }
+
+        // On websocket connected
+        function onConnected() {
+            console.log('Connected to websocket');
+        }
+    </script>
 @endpush
