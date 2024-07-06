00b08accd5501456e3dd917839f1a0ea4bd141f9
Arifia Kasastra R <arka.progammer@gmail.com>
2024-07-06 23:35:51 +0700

    Creating api route for triggering event

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\NewMessage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('trigger', function (Request $request) {
+    NewMessage::dispatch($request->message);
+
+    return response()->json([
+        'message' => 'Event triggered'
+    ]);
 });
